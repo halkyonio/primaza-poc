bf477e32841bbeaaf544c0843b99ffaa7b68e3f3
Charles Moulliard <cmoulliard@redhat.com>
2022-10-17 13:09:46 +0200

    Creating additional resourcs to setup a menu from the main.html page

---
+++ servicebox-app/src/main/java/io/halkyon/rest/Application.java
@@ -0,0 +1,24 @@
+package io.halkyon.rest;
+
+import io.quarkiverse.renarde.Controller;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import io.smallrye.common.annotation.Blocking;
+
+import javax.ws.rs.Path;
+
+@Blocking
+public class Application extends Controller {
+
+    @CheckedTemplate
+    static class Templates {
+        public static native TemplateInstance index();
+        public static native TemplateInstance about();
+    }
+
+    @Path("/")
+    public TemplateInstance index() { return Templates.index(); }
+
+    @Path("/about")
+    public TemplateInstance about() { return Templates.about(); }
+}
---
+++ servicebox-app/src/main/java/io/halkyon/rest/Templates.java
@@ -0,0 +1,9 @@
+package io.halkyon.rest;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+@CheckedTemplate
+public class Templates {
+    public static native TemplateInstance main();
+}
