4f946a43b6b2085e92885d665f057869d7b00a08
Charles Moulliard <cmoulliard@redhat.com>
2022-10-25 18:50:41 +0200

    Add the missing package template to io.halkyon.template.ClaimPage

--- servicebox-app/src/main/java/io/halkyon/service/ClaimResource.java
+++ servicebox-app/src/main/java/io/halkyon/service/ClaimResource.java
@@ -8,7 +8,7 @@ import io.quarkus.rest.data.panache.ResourceProperties;
 /***
  * REST Data with Panache generates JAX-RS resources based on this interface. You don't need to implement it.
  * For the Claim entity this extension will generate its implementation providing support for CRUD operations and `application/json` as response content types.
- * For extending this interface the @see io.halkyon.ClaimPage is provided.
+ * For extending this interface the @see io.halkyon.template.ClaimPage is provided.
  */
 @ResourceProperties(path = "claims")
 public interface ClaimResource extends PanacheEntityResource<Claim, Long> { }
