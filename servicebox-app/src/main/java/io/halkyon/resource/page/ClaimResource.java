0bda226c80a0a03fae07623592f771c356c76b36
Charles Moulliard <cmoulliard@redhat.com>
2022-10-26 18:46:59 +0200

    Moving HTTP Post add method to the claimResource class

--- servicebox-app/src/main/java/io/halkyon/resource/page/ClaimResource.java
+++ servicebox-app/src/main/java/io/halkyon/resource/page/ClaimResource.java
@@ -2,9 +2,11 @@ package io.halkyon.resource.page;
 
 import io.halkyon.Templates;
 import io.halkyon.service.ClaimStatus;
+import io.halkyon.service.ClaimValidator;
 import io.quarkus.qute.TemplateInstance;
 import org.jboss.resteasy.annotations.Form;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,10 +16,42 @@ import java.util.List;
 
 @Path("/claim")
 public class ClaimResource {
+    ClaimValidator claimValidator;
+
+    @Inject
+    public ClaimResource(ClaimValidator claimValidator){
+        this.claimValidator = claimValidator;
+    }
 
     @GET
     public TemplateInstance claim() {
         return Templates.claimForm().data("tile","Claim form");
+    }
+
+    @POST
+    @Transactional
+    @Consumes("application/x-www-form-urlencoded")
+    public Response add(@Form io.halkyon.model.Claim claim, @HeaderParam("HX-Request") boolean hxRequest) {
+        List<String> errors = claimValidator.validateForm(claim);
+        StringBuffer response = new StringBuffer();
+
+        if (claim.created == null) {
+            claim.created = new Date(System.currentTimeMillis());
+        }
+        if (claim.status == null) {
+            claim.status = ClaimStatus.NEW.toString();
+        }
+
+        claim.persist();
+        if (errors.size() > 0) {
+            for(String error : errors) {
+                response.append("<div class=\"alert alert-danger\"><strong>Error! </strong>" + error + "</div>");
+            };
+        } else {
+            response.append("<div class=\"alert alert-success\">Claim created successfully for id: " + claim.id + "</div>");
+        }
+        // Return as HTML the template rendering the item for HTMX
+        return Response.accepted(response.toString()).status(Response.Status.CREATED).header("Location", "/claim").build();
     }
 
 }
--- servicebox-app/src/main/java/io/halkyon/resource/page/ClaimsResource.java
+++ servicebox-app/src/main/java/io/halkyon/resource/page/ClaimsResource.java
@@ -20,13 +20,6 @@ import java.util.Map;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ClaimsResource {
 
-    ClaimValidator claimValidator;
-
-    @Inject
-    public ClaimsResource(ClaimValidator claimValidator){
-        this.claimValidator = claimValidator;
-    }
-
     @GET
     public TemplateInstance list() {
         return showList(io.halkyon.model.Claim.listAll()).data("all", true);
@@ -40,31 +33,5 @@ public class ClaimsResource {
     @Path("/{name}")
     public io.halkyon.model.Claim findByName(@PathParam("name") String name) {
         return io.halkyon.model.Claim.findByName(name);
-    }
-
-    @POST
-    @Transactional
-    @Consumes("application/x-www-form-urlencoded")
-    public Response add(@Form io.halkyon.model.Claim claim, @HeaderParam("HX-Request") boolean hxRequest) {
-        List<String> errors = claimValidator.validateForm(claim);
-        StringBuffer response = new StringBuffer();
-
-        if (claim.created == null) {
-            claim.created = new Date(System.currentTimeMillis());
-        }
-        if (claim.status == null) {
-            claim.status = ClaimStatus.NEW.toString();
-        }
-
-        claim.persist();
-        if (errors.size() > 0) {
-            for(String error : errors) {
-                response.append("<div class=\"alert alert-danger\"><strong>Error! </strong>" + error + "</div>");
-            };
-        } else {
-            response.append("<div class=\"alert alert-success\">Claim created successfully for id: " + claim.id + "</div>");
-        }
-        // Return as HTML the template rendering the item for HTMX
-        return Response.accepted(response.toString()).status(Response.Status.CREATED).header("Location", "/claim").build();
     }
 }
