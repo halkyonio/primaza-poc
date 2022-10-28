88e8182e1830958bd5c617f135be0bd059984b66
Charles Moulliard <cmoulliard@redhat.com>
2022-10-28 13:01:56 +0200

    WIP. Evaluating hyperscript as companion to better read/understand HTML code and JS events

--- servicebox-app/src/main/resources/META-INF/resources/js/primaza.js
+++ servicebox-app/src/main/resources/META-INF/resources/js/primaza.js
@@ -1,16 +1,16 @@
 (() => {
     // EventListener watching submit button event
-    // Clean the inner HTML of the <div id="response"> to remove previously displayed messages
+/*    // Clean the inner HTML of the <div id="response"> to remove previously displayed messages
     document.addEventListener("submit", () => {
         document.getElementById("response").innerHTML = "";
-    });
+    });*/
 
     // Event watching when HTMX is swapping the HTML content
     // If the div tag includes as class "alert-success",, then the button is disabled
-    htmx.on("htmx:afterSwap",function(evt) {
+/*    htmx.on("htmx:afterSwap",function(evt) {
         const msg = evt.detail.elt.outerHTML;
         if (msg.includes("alert-success")) {
             document.getElementById("claim-button").setAttribute("disabled", true);
         };
-    });
+    });*/
 })();
