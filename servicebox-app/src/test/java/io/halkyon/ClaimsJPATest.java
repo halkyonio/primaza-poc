a05c63b2e5d9e72e2419c582c15cc40bb1d91df6
Charles Moulliard <cmoulliard@redhat.com>
2022-11-09 17:02:47 +0100

    Adding test which is failing asquery wusing like do not work

--- servicebox-app/src/test/java/io/halkyon/ClaimsJPATest.java
+++ servicebox-app/src/test/java/io/halkyon/ClaimsJPATest.java
@@ -1,16 +1,19 @@
 package io.halkyon;
 
 import io.halkyon.model.Claim;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.test.junit.QuarkusTest;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @QuarkusTest
 public class ClaimsJPATest {
+
+    public class ClaimRepository implements PanacheRepository<Claim> {
+    }
 
     @Test
     public void testQueryByName() {
@@ -28,5 +31,30 @@ public class ClaimsJPATest {
 
         // then
         MatcherAssert.assertThat(claims, Matchers.hasSize(1));
+    }
+
+    @Test
+    public void testUsingRepositoryAndQuery() {
+        String name = "mysql-demo";
+        String serviceRequested= "";
+        ClaimRepository cr = new ClaimRepository();
+
+        String query = "name = ?1 or servicerequested = ?2";
+        List<Claim> claims = cr.list(query, name, serviceRequested);
+
+        MatcherAssert.assertThat(claims, Matchers.hasSize(1));
+    }
+
+    // Test is failing using like :-(
+    @Test
+    public void testUsingRepositoryAndQueryWithLike() {
+        String name = "mysql";
+        String serviceRequested= "";
+        ClaimRepository cr = new ClaimRepository();
+
+        String query = "name like ?1 or servicerequested = ?2";
+        List<Claim> claims = cr.list(query, name, serviceRequested);
+
+        MatcherAssert.assertThat(claims, Matchers.hasSize(0));
     }
 }
