766f3de3003d04968e8706e60039114e3eb613d3
Charles Moulliard <cmoulliard@redhat.com>
2022-11-10 16:45:43 +0100

    Declare the javax.persistence packages to be used instead of importing all

--- servicebox-app/src/main/java/io/halkyon/model/Service.java
+++ servicebox-app/src/main/java/io/halkyon/model/Service.java
@@ -6,7 +6,13 @@ import io.quarkus.panache.common.Sort;
 import io.quarkus.security.credential.Credential;
 import org.jboss.resteasy.annotations.jaxrs.FormParam;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import java.sql.Date;
 import java.util.List;
