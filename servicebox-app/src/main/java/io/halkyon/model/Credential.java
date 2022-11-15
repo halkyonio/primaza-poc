fef8f371d9a83d4f7a086f9d404c480d5b3b0c6b
Jose <josecarvajalhilario@gmail.com>
2022-11-15 11:48:43 +0100

    Use the identity generator identity for credential entities

--- servicebox-app/src/main/java/io/halkyon/model/Credential.java
+++ servicebox-app/src/main/java/io/halkyon/model/Credential.java
@@ -6,11 +6,11 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -20,8 +20,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 public class Credential extends PanacheEntityBase {
 
     @Id
-    @SequenceGenerator(name = "creSeq", sequenceName = "cre_id_seq", allocationSize = 1, initialValue = 5)
-    @GeneratedValue(generator = "creSeq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public String name;
--- servicebox-app/src/main/java/io/halkyon/model/CredentialParameter.java
+++ servicebox-app/src/main/java/io/halkyon/model/CredentialParameter.java
@@ -2,10 +2,10 @@ package io.halkyon.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -15,8 +15,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 public class CredentialParameter extends PanacheEntityBase {
 
     @Id
-    @SequenceGenerator(name = "creParamSeq", sequenceName = "cre_param_id_seq", allocationSize = 1, initialValue = 5)
-    @GeneratedValue(generator = "creParamSeq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String paramName;
     public String paramValue;
