92d9e11ad8e673537bda3a92ce88de6e313e5ba3
Jose <josecarvajalhilario@gmail.com>
2022-11-15 11:48:43 +0100

    Use the identity generator identity for credential entities

--- servicebox-app/src/main/java/io/halkyon/model/Service.java
+++ servicebox-app/src/main/java/io/halkyon/model/Service.java
@@ -11,7 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import java.sql.Date;
