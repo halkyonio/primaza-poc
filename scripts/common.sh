f0719f54dfee4b91b63761d5ee363c61fde30e1b
Charles Moulliard <cmoulliard@redhat.com>
2022-11-17 14:33:49 +0100

    Created bash script to install kind + reg + ingress + primaza (#107)
    * Created bash script to install kind + reg + ingres + primaza
    
    * Removing non needed code to clone the project as it should be done externally
    
    * Adding variables to play the demo
    
    * Adding missing $ symbol in front of the var
    
    * Configure the scripts path and use pushd/popd
    
    * Logging the commands executed
    
    * Changing the play demo speed and moving some vars
    
    * Allwoing to override the TYPE_SPEED VAR
    
    * Replacing the var  with
    
    * Adding the missing and needed -n NAMESPACE to the kubectl command
    
    * Remove the hard coded namespace and use the $NAMESPACE variable
    
    * Improve kubectl commands readability
    
    * Documenting how to install/configure k8s cluster to play with Primaza

---
+++ scripts/common.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -e
+
+shopt -s expand_aliases
+alias k='kubectl'
+
+# Defining some colors for output
+NC='\033[0m' # No Color
+COLOR_RESET="\033[0m" # Reset color
+BLACK="\033[0;30m"
+BLUE='\033[0;34m'
+BROWN="\033[0;33m"
+GREEN='\033[0;32m'
+GREY="\033[0;90m"
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+RED='\033[0;31m'
+PURPLE="\033[0;35m"
+WHITE='\033[0;37m'
+YELLOW='\033[0;33m'
+
+shopt -s expand_aliases
+alias k='kubectl'
+
+####################################
+## Section to declare the functions
+####################################
+repeat_char(){
+  COLOR=${1}
+	for i in {1..70}; do echo -ne "${!COLOR}$2${NC}"; done
+}
+
+msg() {
+  COLOR=${1}
+  MSG="${@:2}"
+  echo -e "\n${!COLOR}## ${MSG}${NC}"
+}
+
+succeeded() {
+  echo -e "${GREEN}NOTE:${NC} $1"
+}
+
+note() {
+  echo -e "${BLUE}NOTE:${NC} $1"
+}
+
+warn() {
+  echo -e "${YELLOW}WARN:${NC} $1"
+}
+
+error() {
+  echo -e "${RED}ERROR:${NC} $1"
+}
+
+log() {
+  MSG="${@:2}"
+  echo; repeat_char ${1} '#'; msg ${1} ${MSG}; repeat_char ${1} '#'; echo
+}
---
+++ scripts/primaza.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR="$(cd $(dirname "${BASH_SOURCE}") && pwd)"
+
+source ${SCRIPTS_DIR}/common.sh
+source ${SCRIPTS_DIR}/play-demo.sh
+
+####################################
+## Variables
+####################################
+useConfirm=true
+VM_IP=${VM_IP:=127.0.0.1}
+NAMESPACE=primaza
+REGISTRY_GROUP=local
+REGISTRY=kind-registry:5000
+IMAGE_VERSION=latest
+INGRESS_HOST=primaza.${VM_IP}.nip.io
+PROJECT_DIR=servicebox-app
+
+# Parameters to play the demo
+TYPE_SPEED=${TYPE_SPEED:=40}
+NO_WAIT=true
+
+pushd ${PROJECT_DIR}
+
+p "SCRIPTS_DIR dir: ${SCRIPTS_DIR}"
+p "Ingress host is: ${INGRESS_HOST}"
+
+pe "curl -s -L "https://raw.githubusercontent.com/snowdrop/k8s-infra/main/kind/kind-reg-ingress.sh" | bash -s y latest 0"
+pe "k wait -n ingress \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s"
+
+pe "k create namespace ${NAMESPACE}"
+pe "k config set-context --current --namespace=${NAMESPACE}"
+
+pe "mvn clean install -DskipTests -Ppush-images,kubernetes -Dquarkus.container-image.build=true \
+   -Dquarkus.container-image.push=true \
+   -Dquarkus.container-image.registry=${REGISTRY} \
+   -Dquarkus.container-image.group=${REGISTRY_GROUP} \
+   -Dquarkus.container-image.tag=${IMAGE_VERSION} \
+   -Dquarkus.container-image.insecure=true \
+   -Dquarkus.kubernetes.ingress.host=${INGRESS_HOST}"
+
+pe "kind load docker-image ${REGISTRY}/${REGISTRY_GROUP}/servicebox-app"
+
+pe "helm install --devel servicebox-app \
+  --dependency-update \
+  ./target/helm/kubernetes/servicebox-app \
+  -n ${NAMESPACE} \
+  --set app.image=localhost:5000/${REGISTRY_GROUP}/servicebox-app:${IMAGE_VERSION} 2>&1 1>/dev/null"
+
+pe "k wait -n ${NAMESPACE} \
+  --for=condition=ready pod \
+  -l app.kubernetes.io/name=servicebox-app \
+  --timeout=7m"
+
+p "waiting till Primaza Application is running"
+POD_NAME=$(k get pod -l app.kubernetes.io/name=servicebox-app -n ${NAMESPACE} -o name)
+while [[ $(k exec -i $POD_NAME -c servicebox-app -n ${NAMESPACE} -- bash -c "curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/home") != "200" ]];
+  do sleep 1
+done
+
+p "Get the kubeconf and creating a cluster"
+KIND_URL=https://kubernetes.default.svc
+kind get kubeconfig > local-kind-kubeconfig
+pe "k cp local-kind-kubeconfig ${NAMESPACE}/${POD_NAME:4}:/tmp/local-kind-kubeconfig -c servicebox-app"
+
+RESULT=$(k exec -i $POD_NAME -c servicebox-app -n ${NAMESPACE} -- sh -c "curl -X POST -H 'Content-Type: multipart/form-data' -H 'HX-Request: true' -F name=local-kind -F environment=DEV -F url=$KIND_URL -F kubeConfig=@/tmp/local-kind-kubeconfig -s -i localhost:8080/clusters")
+if [ "$RESULT" = *"500 Internal Server Error"* ]
+then
+    p "Cluster failed to be saved in Service Box: $RESULT"
+    pe "k describe $POD_NAME -n ${NAMESPACE}"
+    pe "k logs $POD_NAME -n ${NAMESPACE}"
+    exit 1
+fi
+p "Local k8s cluster registered: $RESULT"
+
+popd
