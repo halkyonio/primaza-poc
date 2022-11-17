720cb9f4382e8874ff7a8aa53ab2f1db05d03ece
Charles Moulliard <cmoulliard@redhat.com>
2022-11-17 15:35:50 +0100

    Remove echo command and curl in silence (#114)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 PROJECT_NAME=primaza-poc
 rm -rf ${PROJECT_NAME}
-curl https://github.com/halkyonio/primaza-poc/archive/refs/heads/main.zip
+curl -s https://github.com/halkyonio/primaza-poc/archive/refs/heads/main.zip
 unzip main.zip -d ${PROJECT_NAME}
 
 cd ${PROJECT_NAME}
-echo "VM_IP=$1 ./scripts/primaza.sh"
+VM_IP=$1 ./scripts/primaza.sh
