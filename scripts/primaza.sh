7c1a26d1e4bdf92b29e4f83229882006f606a7fd
Charles Moulliard <cmoulliard@redhat.com>
2022-11-17 15:18:44 +0100

    Add var CLONING_PRIMAZA to git clone the project (#112)

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -15,11 +15,19 @@ REGISTRY_GROUP=local
 REGISTRY=kind-registry:5000
 IMAGE_VERSION=latest
 INGRESS_HOST=primaza.${VM_IP}.nip.io
+PROJECT_NAME=primaza-poc
 PROJECT_DIR=servicebox-app
+CLONING_PRIMAZA=${CLONING_PRIMAZA:=false}
+GITHUB_REPO_PRIMAZA=https://github.com/halkyonio/primaza-poc.git
 
 # Parameters to play the demo
 TYPE_SPEED=${TYPE_SPEED:=40}
 NO_WAIT=true
+
+if [ "${CLONING_PRIMAZA}" = true ]; then
+  pe "rm -rf ${PROJECT_NAME}"
+  pe "git clone ${GITHUB_REPO_PRIMAZA} && cd ${PROJECT_NAME}"
+fi
 
 pushd ${PROJECT_DIR}
 
