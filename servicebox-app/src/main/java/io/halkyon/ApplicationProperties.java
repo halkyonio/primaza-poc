b051c6bfb45d8d5ce23c8b9c774819fa7445cb78
Charles Moulliard <cmoulliard@redhat.com>
2022-11-18 15:19:49 +0100

    Removing TemplateGlobal as we cannot injct Config to a static property and using instead {config} withn the html template

--- servicebox-app/src/main/java/io/halkyon/ApplicationProperties.java
+++ servicebox-app/src/main/java/io/halkyon/ApplicationProperties.java
@@ -1,16 +1,15 @@
 package io.halkyon;
 
-import io.quarkus.qute.TemplateGlobal;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 
 @ApplicationScoped
-@TemplateGlobal
 public class ApplicationProperties {
 
     @ConfigProperty(name = "github.repo", defaultValue = "http://github.com/halkyonio/primaza-poc")
-    static String gGitHubRepo;
+    public String gGitHubRepo;
     @ConfigProperty(name = "git.sha.commit", defaultValue = "666")
-    static String gGitShaCommit;
+    public String gGitShaCommit;
 }
