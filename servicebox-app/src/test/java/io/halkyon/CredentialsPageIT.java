a3cce7879277447b2c605fa27d11d6d82151e519
Auri Munoz <amunozhe@redhat.com>
2022-11-23 15:10:32 +0100

    skip only the test method affected

--- servicebox-app/src/test/java/io/halkyon/CredentialsPageIT.java
+++ servicebox-app/src/test/java/io/halkyon/CredentialsPageIT.java
@@ -4,6 +4,5 @@ import io.quarkus.test.junit.DisabledOnIntegrationTest;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-@DisabledOnIntegrationTest
 public class CredentialsPageIT extends CredentialsPageTest {
 }
