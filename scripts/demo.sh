9f6bdea54cb5775ab5cece6ef8e587d9d49c123a
Charles Moulliard <cmoulliard@redhat.com>
2022-11-24 10:05:40 +0100

    Create the script to build, install the quarkus super hero application on k8s

---
+++ scripts/demo.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR="$(cd $(dirname "${BASH_SOURCE}") && pwd)"
+
+source ${SCRIPTS_DIR}/common.sh
+source ${SCRIPTS_DIR}/play-demo.sh
+
+####################################
+## Variables
+####################################
+VM_IP=${VM_IP:=127.0.0.1}
+NAMESPACE=app
+REGISTRY_GROUP=local
+REGISTRY=kind-registry:5000
+IMAGE_VERSION=latest
+INGRESS_HOST=superheroes.${VM_IP}.nip.io
+PROJECT_NAME=quarkus-super-heroes
+PROJECT_DIR=rest-heroes
+GITHUB_REPO=https://github.com/quarkusio/quarkus-super-heroes
+
+# Parameters to play the demo
+TYPE_SPEED=${TYPE_SPEED:=40}
+NO_WAIT=true
+
+if [[ -d "${PROJECT_NAME}" ]]; then
+  git clone ${GITHUB_REPO}
+fi
+
+pushd ${PROJECT_NAME}/${PROJECT_DIR}
+
+pe "kubectl create namespace $NAMESPACE"
+
+pe "mvn quarkus:add-extension -Dextensions=\"quarkus-kubernetes-service-binding\""
+p "Remove the third party installations via templates (we'll install these services using Service Box :) )"
+pe "rm -rf src/main/kubernetes"
+p "Remove the default application.yml as we'll provide a different one with our Helm properties"
+pe "rm -rf src/main/resources/application.yml"
+p "Copy the import.sql file"
+pe "cp deploy/db-init/initialize-tables.sql src/main/resources/"
+
+pe "cat > src/main/resources/application.properties << EOF
+quarkus.application.name=rest-heroes
+quarkus.http.port=8080
+quarkus.hibernate-orm.sql-load-script=initialize-tables.sql
+quarkus.hibernate-orm.database.generation=drop-and-create
+quarkus.container-image.build=true
+quarkus.container-image.builder=docker
+quarkus.container-image.group=superhero
+quarkus.container-image.tag=1.0
+quarkus.kubernetes.deployment-target=kubernetes
+EOF"
+
+p "Package the application and build the image"
+pe "mvn clean package -DskipTests \
+  -Dquarkus.container-image.push=true \
+  -Dquarkus.container-image.registry=$KIND_REGISTRY \
+  -Dquarkus.kubernetes.namespace=$NAMESPACE \
+  -Dquarkus.kubernetes.deploy=true"
