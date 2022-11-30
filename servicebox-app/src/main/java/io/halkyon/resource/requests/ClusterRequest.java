0f0eb14b3b1f8f824fbdc92620a318ac2bc7ecf0
Charles Moulliard <cmoulliard@redhat.com>
2022-11-30 15:17:20 +0100

    Add a message explaining the purpose of the namespaces of the cluster's form

--- servicebox-app/src/main/java/io/halkyon/resource/requests/ClusterRequest.java
+++ servicebox-app/src/main/java/io/halkyon/resource/requests/ClusterRequest.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import javax.validation.constraints.NotBlank;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.annotations.jaxrs.FormParam;
