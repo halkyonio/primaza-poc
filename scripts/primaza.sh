e608f5dcc8103955eec9b648c5195e76cbdcc531
Charles Moulliard <cmoulliard@redhat.com>
2022-12-07 17:09:24 +0100

    Set the log level to DEBUG for io.fabric8.kubernetes.client

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -44,7 +44,7 @@ pe "mvn clean install -DskipTests -Ppush-images,kubernetes -Dquarkus.container-i
    -Dquarkus.container-image.tag=${IMAGE_VERSION} \
    -Dquarkus.container-image.insecure=true \
    -Dquarkus.kubernetes.ingress.host=${INGRESS_HOST} \
-   -Dlog.level=debug \
+   -Dlog.level=INFO \
    -Dgit.sha.commit=$(git rev-parse --short HEAD) \
    -Dgithub.repo=https://github.com/halkyonio/primaza-poc"
 
