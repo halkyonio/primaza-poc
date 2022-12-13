992ea8664699980002cc6cadf7b26970a3dcb0c4
Jose <josecarvajalhilario@gmail.com>
2022-12-13 10:44:23 +0100

    Fix cascade issues when deleting a cluster entity
    Fix https://github.com/halkyonio/primaza-poc/issues/196

--- servicebox-app/src/test/java/io/halkyon/ClustersEndpointTest.java
+++ servicebox-app/src/test/java/io/halkyon/ClustersEndpointTest.java
@@ -1,5 +1,8 @@
 package io.halkyon;
 
+import static io.halkyon.utils.TestUtils.createCluster;
+import static io.halkyon.utils.TestUtils.createService;
+import static io.halkyon.utils.TestUtils.mockServiceIsAvailableInCluster;
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -10,14 +13,20 @@ import javax.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
 
 import io.halkyon.model.Cluster;
+import io.halkyon.model.Service;
 import io.halkyon.services.KubernetesClientService;
+import io.halkyon.utils.WebPageExtension;
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @QuarkusTest
+@QuarkusTestResource(WebPageExtension.class)
 public class ClustersEndpointTest {
+
+    WebPageExtension.PageManager page;
 
     @InjectMock
     KubernetesClientService mockKubernetesClientService;
@@ -37,5 +46,24 @@ public class ClustersEndpointTest {
                 .statusCode(201);
     }
 
+    @Test
+    public void testDeleteClusterInPage() {
+        // First, we create a cluster with a service
+        String prefix = "ClustersEndpointTest-testDeleteClusterInPage-";
+        Cluster cluster = createCluster(prefix + "cluster", "master:port");
+        mockServiceIsAvailableInCluster(mockKubernetesClientService, cluster.name, "testDeleteClusterInPage", "1111", "ns1");
+        createService(prefix + "service", "Api", "any", "demo", "testDeleteClusterInPage:1111");
+
+        // When, we go to the clusters page
+        page.goTo("/clusters");
+        page.assertContentContains(cluster.name);
+
+        // And click on delete
+        page.clickById("btn-cluster-delete-" + cluster.id);
+
+        // Then, the cluster should have been deleted
+        page.goTo("/clusters");
+        page.assertContentDoesNotContain(cluster.name);
+    }
 
 }
