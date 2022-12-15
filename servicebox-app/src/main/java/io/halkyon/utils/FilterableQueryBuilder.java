6e78066a4940c19f7f49777d5d0ec304ba14b648
Jose <josecarvajalhilario@gmail.com>
2022-12-15 13:44:19 +0100

    Add filters for credentials page
    Relates https://github.com/halkyonio/primaza-poc/issues/45

--- servicebox-app/src/main/java/io/halkyon/utils/FilterableQueryBuilder.java
+++ servicebox-app/src/main/java/io/halkyon/utils/FilterableQueryBuilder.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import io.quarkus.panache.common.Parameters;
@@ -15,22 +16,25 @@ public class FilterableQueryBuilder {
     private final Parameters parameters = new Parameters();
     private final List<String> query = new LinkedList<>();
 
-    public void equals(String param, Object value) {
-        query.add(param + "=:" + param);
+    public void equals(String field, Object value) {
+        String param = toParamName(field);
+        query.add(field + "=:" + param);
         parameters.and(param, value);
-        filter.put(param, value);
+        filter.put(field, value);
     }
 
-    public void containsIgnoreCase(String param, String value) {
-        query.add("LOWER(" + param + ") like :" + param);
+    public void containsIgnoreCase(String field, String value) {
+        String param = toParamName(field);
+        query.add("LOWER(" + field + ") like :" + param);
         parameters.and(param, "%" + value.toLowerCase(Locale.ROOT) + "%");
-        filter.put(param, value);
+        filter.put(field, value);
     }
 
-    public void startsWith(String param, String value) {
-        query.add(param + " like :" + param);
+    public void startsWith(String field, String value) {
+        String param = toParamName(field);
+        query.add(field + " like :" + param);
         parameters.and(param, value + "%");
-        filter.put(param, value);
+        filter.put(field, value);
     }
 
     public String build() {
@@ -43,5 +47,10 @@ public class FilterableQueryBuilder {
 
     public Map<String, Object> getFilterAsMap() {
         return Collections.unmodifiableMap(filter);
+    }
+
+    private String toParamName(String param) {
+        // remove dots
+        return param.replaceAll(Pattern.quote("."), "");
     }
 }
