ffb97c3e954203caca4851b896b129a2727850c7
Charles Moulliard <cmoulliard@redhat.com>
2023-03-13 17:52:02 +0100

    Fixing the issue as the vault client was not able to access the server and improving the log of the messages
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/vault.sh
+++ scripts/vault.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-# TODO: To be improved using: https://github.com/redhat-appstudio/service-provider-integration-operator/blob/main/hack/vault-init.sh
-
 SCRIPTS_DIR="$(cd $(dirname "${BASH_SOURCE}") && pwd)"
-
 source ${SCRIPTS_DIR}/common.sh
 
+####################
+## Global parameters
+#####################
 VM_IP=${VM_IP:-127.0.0.1}
-USER=${0:-bob}
-PASSWORD=${1:-sinclair}
-PATH_PREFIX=${2:-kv/*}
-APP=${2:-primaza}
 
-echo "USER 1: ${USER}"
-echo "PASSWORD 2: ${PASSWORD}"
-echo "PATH_PREFIX 3: ${PATH_PREFIX}"
-echo "APP 4: ${APP}"
+VAULT_USER=${VAULT_USER:-bob}
+VAULT_PASSWORD=${VAULT_PASSWORD:-sinclair}
+APP=${APP_POLICY:-primaza}
+KV_PREFIX=${KEY_PATH:-kv}
 
+#########################
+## Generic functions
+#########################
+function vaultExec() {
+  COMMAND=${1}
+  kubectl exec vault-0 -n vault -- sh -c "${COMMAND}" 2> /dev/null
+}
 
 function install() {
-  echo "Installing Vault Helm"
+  log BLUE "Installing Vault Helm"
   cat <<EOF > ./my-values.yml
 server:
   updateStrategyType: RollingUpdate
@@ -38,29 +41,28 @@ EOF
   helm install vault hashicorp/vault --create-namespace -n vault -f ./my-values.yml
 }
 
+#########################
+## Script functions
+#########################
 function remove() {
-  echo "Removing helm vault & pvc"
+  log BLUE "Removing helm vault & pvc"
   helm uninstall vault -n vault
   kubectl delete pvc -n vault -lapp.kubernetes.io/name=vault
 }
 
-function vaultExec() {
-  COMMAND=${1}
-  kubectl exec vault-0 -n vault -- sh -c "${COMMAND}" 2> /dev/null
-}
-
 function login() {
-  echo "Logging in as Root"
+  log BLUE "Logging in as Root"
   ROOT_TOKEN=$(jq -r ".root_token" ./cluster-keys.json)
-  echo "Root Token: ${ROOT_TOKEN}"
   vaultExec "vault login ${ROOT_TOKEN}"
 }
 
 function loginAsUser() {
+  log BLUE "Login as user: ${VAULT_USER}"
   vaultExec "vault login -method=userpass username=bob password=sinclair"
 }
 
 function unseal() {
+    log BLUE "Init vault and unseal"
     vaultExec "vault operator init \
         -key-shares=1 \
         -key-threshold=1 \
@@ -68,48 +70,52 @@ function unseal() {
 
     VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" ./cluster-keys.json)
     vaultExec "vault operator unseal $VAULT_UNSEAL_KEY"
-    echo "##############################################################"
-    echo "Vault Root Token: $(jq -r ".root_token" ./cluster-keys.json)"
-    echo "##############################################################"
+
+    log YELLOW "Vault Root Token: $(jq -r ".root_token" ./cluster-keys.json)"
 }
 
-function enableKvSecretEngine() {
+function enableKVSecretEngine() {
+  log BLUE "Enable KV secret engine"
   vaultExec "vault secrets enable kv"
 }
 
 function enableK8sSecretEngine() {
+  log BLUE "Enable Kubernetes secret engine"
   vaultExec "vault secrets enable kubernetes"
 }
 
 function enableUserPasswordAuth() {
+  log BLUE "Enable User/password auth"
   vaultExec "vault auth enable userpass"
 }
 
 function registerUser() {
-  vaultExec "vault write auth/userpass/users/${1} password=${2} policies=kv-${3}-policy"
+  POLICY_NAME=${KV_PREFIX}-${APP_POLICY}-policy
+  note "User: ${VAULT_USER}"
+  note "Password: ${VAULT_PASSWORD}"
+  note "Policy name: POLICY_NAME"
+  vaultExec "vault write auth/userpass/users/${VAULT_USER} password=${VAULT_PASSWORD} policies=${POLICY_NAME}"
 }
 
 function createUserPolicy() {
-  echo "Creating User Policy"
-  PATH_PREFIX=${1}
-  APP=${2}
-  echo "Creating kv-${APP}-policy User Policy for '${PATH_PREFIX}' path"
-  cat <<EOF | vault policy write kv-$APP-policy -
-path "${PATH_PREFIX}" {
-  capabilities = ["read", "create"]
-}
-EOF
+  POLICY_NAME=${KV_PREFIX}-${APP_POLICY}-policy
+  ROLES="\"read\", \"create\", \"list\", \"delete\", \"update\""
+  log BLUE "Creating policy ${POLICY_NAME} for path: \"${KV_PREFIX}\"${APP}\"* having as roles: "
 
+  POLICY_FILE=/tmp/spi_policy.hcl
+  vaultExec "echo 'path \"${KV_PREFIX}/${APP}*\" { capabilities = [${ROLES}] }' > ${POLICY_FILE}"
+  vaultExec "vault policy write POLICY_NAME $POLICY_FILE"
 }
 
 case $1 in
     install) "$@"; exit;;
     remove) "$@"; exit;;
     unseal) "$@"; exit;;
-    kv) "$@"; exit;;
+    enableKVSecretEngine) "$@"; exit;;
+    enableK8sSecretEngine) "$@"; exit;;
     login) "$@"; exit;;
-    createUserPolicy) "$@"; exit;;
     enableUserPasswordAuth) "$@"; exit;;
+    createUserPolicy) "$@"; exit;;
     registerUser) "$@"; exit;;
     loginAsUser) "$@"; exit;;
 esac
@@ -119,9 +125,9 @@ install
 sleep 20
 unseal
 login
-enableKvSecretEngine
+enableKVSecretEngine
 enableK8sSecretEngine
-createUserPolicy ${PATH_PREFIX} ${APP}
 enableUserPasswordAuth
-registerUser ${USER} ${PASSWORD} kv-${APP}-policy
+createUserPolicy
+registerUser
 loginAsUser
