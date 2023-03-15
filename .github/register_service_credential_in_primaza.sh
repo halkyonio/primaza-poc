57ed89c4fe8d574664251d9e009bc44c7b3c5872
Auri Munoz <amunozhe@redhat.com>
2023-03-15 17:51:17 +0100

    test credential from vault

--- .github/register_service_credential_in_primaza.sh
+++ .github/register_service_credential_in_primaza.sh
@@ -5,6 +5,7 @@ SERVICE_NAME=$2
 USERNAME=$3
 PASSWORD=$4
 DATABASE_NAME=$5
+VAULT_KV_PATH=$6
 
 PRIMAZA_KUBERNETES_NAMESPACE=sb
 POD_NAME=$(kubectl get pod -l app.kubernetes.io/name=primaza-app -n $PRIMAZA_KUBERNETES_NAMESPACE -o name)
@@ -12,7 +13,8 @@ POD_NAME=$(kubectl get pod -l app.kubernetes.io/name=primaza-app -n $PRIMAZA_KUB
 SERVICE=$(kubectl exec -i $POD_NAME --container primaza-app -n $PRIMAZA_KUBERNETES_NAMESPACE -- sh -c "curl -H 'Accept: application/json' -s localhost:8080/services/name/$SERVICE_NAME")
 SERVICE_ID=$(echo "$SERVICE" | jq -r '.id')
 
-BODY="name=$CREDENTIAL_NAME&serviceId=$SERVICE_ID&username=$USERNAME&password=$PASSWORD&params=database:$DATABASE_NAME"
+#BODY="name=$CREDENTIAL_NAME&serviceId=$SERVICE_ID&username=$USERNAME&password=$PASSWORD&params=database:$DATABASE_NAME"
+BODY="name=$CREDENTIAL_NAME&serviceId=$SERVICE_ID&vaultKvPath=$VAULT_KV_PATH&params=database:$DATABASE_NAME"
 echo "Sending service credential with body: $BODY"
 RESULT=$(kubectl exec -i $POD_NAME --container primaza-app -n $PRIMAZA_KUBERNETES_NAMESPACE -- sh -c "curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H 'HX-Request: true' -d '$BODY' -s -i localhost:8080/credentials")
 if [[ "$RESULT" = *"500 Internal Server Error"* ]]
