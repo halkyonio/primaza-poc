c690fab00355d69da92a2e5210cf911078d093db
Charles Moulliard <cmoulliard@redhat.com>
2023-03-16 11:33:30 +0100

    Logging the root token
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/vault.sh
+++ scripts/vault.sh
@@ -80,7 +80,7 @@ function login() {
 }
 
 function rootToken() {
-  kubectl get secret -n vault tokens -ojson | jq -r '.data.root_token' | base64 -d
+  warn "Vault root token: $(kubectl get secret -n vault tokens -ojson | jq -r '.data.root_token' | base64 -d)"
 }
 
 function loginAsUser() {
