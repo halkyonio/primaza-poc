5ee036e2fe31c75ba8ffb2196fa097306307dbcb
Charles Moulliard <cmoulliard@redhat.com>
2023-03-22 16:36:53 +0100

    Add a new function to the ./scriprs/primaza..sh script to deploy primaza using a helm chart and image pushed on quay.io/halkyonio/primaza-poc without the need to built the project and push the image to a local registry
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -11,11 +11,18 @@ source ${SCRIPTS_DIR}/play-demo.sh
 useConfirm=true
 VM_IP=${VM_IP:=127.0.0.1}
 NAMESPACE=primaza
+PROJECT_DIR=app
+
+# Parameters to be used when we build and push to a local container registry
 REGISTRY_GROUP=local
 REGISTRY=kind-registry:5000
 IMAGE_VERSION=latest
 INGRESS_HOST=primaza.${VM_IP}.nip.io
-PROJECT_DIR=app
+
+# Parameters used when using the image from an external container registry: quay.io/halkyonio/primaza-app
+# and helm chart published on: http://halkyonio.github.io/primaza-helm
+HALKYONIO_HELM_REPO=https://halkyonio.github.io/helm-charts/
+PRIMAZA_IMAGE_NAME=quay.io/halkyonio/primaza-app:0.0.1-SNAPSHOT
 
 # Parameters to play the demo
 TYPE_SPEED=${TYPE_SPEED:=40}
@@ -52,6 +59,46 @@ function build() {
 function deploy() {
     pe "k create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -"
     pe "k config set-context --current --namespace=${NAMESPACE}"
+    pe "helm install \
+      --devel \
+      --repo ${HALKYONIO_HELM_REPO} \
+      primaza-app \
+      primaza-app \
+      -n ${NAMESPACE} \
+      --set app.image=${PRIMAZA_IMAGE_NAME} \
+      --set app.host=\"${INGRESS_HOST}\" \
+      2>&1 1>/dev/null"
+
+    pe "k wait -n ${NAMESPACE} \
+      --for=condition=ready pod \
+      -l app.kubernetes.io/name=primaza-app \
+      --timeout=7m"
+
+    p "waiting till Primaza Application is running"
+    POD_NAME=$(k get pod -l app.kubernetes.io/name=primaza-app -n ${NAMESPACE} -o name)
+    while [[ $(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- bash -c "curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/home") != "200" ]];
+      do sleep 1
+    done
+
+    p "Get the kubeconf and creating a cluster"
+    KIND_URL=https://kubernetes.default.svc
+    pe "kind get kubeconfig -n primaza > local-kind-kubeconfig"
+    pe "k cp local-kind-kubeconfig ${NAMESPACE}/${POD_NAME:4}:/tmp/local-kind-kubeconfig -c primaza-app"
+
+    RESULT=$(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- sh -c "curl -X POST -H 'Content-Type: multipart/form-data' -H 'HX-Request: true' -F name=local-kind -F excludedNamespaces=default,kube-system,ingress,pipelines-as-code,tekton-pipelines,tekton-pipelines-resolvers,vault -F environment=DEV -F url=$KIND_URL -F kubeConfig=@/tmp/local-kind-kubeconfig -s -i localhost:8080/clusters")
+    if [ "$RESULT" = *"500 Internal Server Error"* ]
+    then
+        p "Cluster failed to be saved in Primaza: $RESULT"
+        k describe $POD_NAME -n ${NAMESPACE}
+        k logs $POD_NAME -n ${NAMESPACE}
+        exit 1
+    fi
+    note "Local k8s cluster registered: $RESULT"
+}
+
+function localDeploy() {
+    pe "k create namespace ${NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -"
+    pe "k config set-context --current --namespace=${NAMESPACE}"
     pe "helm install --devel primaza-app \
       --dependency-update \
       ${PROJECT_DIR}/target/helm/kubernetes/primaza-app \
@@ -86,16 +133,16 @@ function deploy() {
 }
 
 function remove() {
-  pe "helm uninstall primaza-app -n ${NAMESPACE}"
+  pe "helm uninstall primaza-app -n ${NAMESPACE}" || true
 }
 
 case $1 in
     install_kind) "$@"; exit;;
     build)        "$@"; exit;;
     deploy)       "$@"; exit;;
+    localDeploy) "$@"; exit;;
     remove)       "$@"; exit;;
 esac
 
 remove
-build
 deploy
