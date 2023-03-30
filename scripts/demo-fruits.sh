44682734b53f013a8161a076b8fed7ee32e6392b
Charles Moulliard <cmoulliard@redhat.com>
2023-03-30 09:34:44 +0200

    Disable the env DB Service name as we will use primaza to define the datasource/binding. Us helm upgrade upgrade to avoid that helm report an error if therepo is already installed. Rename srvice-binding to serviceBinding as helm do not support names with dashes (#292)
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/demo-fruits.sh
+++ scripts/demo-fruits.sh
@@ -11,6 +11,7 @@ source ${SCRIPTS_DIR}/play-demo.sh
 VM_IP=${VM_IP:=127.0.0.1}
 NAMESPACE=app
 FRUITS_CHART_NAME=fruits-app
+HELM_APP_NAME=atomic-fruits
 IMAGE=quay.io/halkyonio/atomic-fruits:latest
 INGRESS_HOST=atomic-fruits.${VM_IP}.nip.io
 
@@ -19,15 +20,18 @@ TYPE_SPEED=${TYPE_SPEED:=200}
 NO_WAIT=true
 
 if [[ "$1" == "clean" ]]; then
-  pe "helm uninstall ${FRUITS_CHART_NAME} -n ${NAMESPACE} || true"
+  pe "helm uninstall ${HELM_APP_NAME} -n ${NAMESPACE} || true"
   exit 0
 fi
 
 pe "k create ns $NAMESPACE --dry-run=client -o yaml | k apply -f -"
 pe "k config set-context --current --namespace=${NAMESPACE}"
 
-pe "helm repo add halkyonio http://halkyonio.github.io/helm-charts"
-pe "helm install ${FRUITS_CHART_NAME} halkyonio/fruits-app \
+pe "helm upgrade -i ${HELM_APP_NAME} \
+    ${FRUITS_CHART_NAME} \
+    --repo http://halkyonio.github.io/helm-charts \
     -n ${NAMESPACE} \
     --set app.image=${IMAGE} \
-    --set app.host=${INGRESS_HOST}"
+    --set app.host=${INGRESS_HOST} \
+    --set app.serviceBinding.enabled=false \
+    --set app.envs.DB_SERVICE_NAME="
