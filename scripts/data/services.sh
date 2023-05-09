c25168741ff5b61345dd43ae1420c4d79e991a21
Charles Moulliard <cmoulliard@redhat.com>
2023-05-09 11:56:23 +0200

    Create an env var to enable/disable the checkbox: installable
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/data/services.sh
+++ scripts/data/services.sh
@@ -19,10 +19,12 @@ NO_WAIT=true
 
 # Script parameters
 PRIMAZA_URL=${PRIMAZA_URL:-localhost:8080}
+INSTALLABLE=${INSTALLABLE-off}
+
 p "Primaza server: ${PRIMAZA_URL}"
 
 declare -a arr=(
-  "name=postgresql&version=14.5&type=postgresql&endpoint=tcp:5432&installable=on&helmRepo=https://charts.bitnami.com/bitnami&helmChart=postgresql&helmChartVersion=11.9.1"
+  "name=postgresql&version=14.5&type=postgresql&endpoint=tcp:5432&installable=$INSTALLABLE&helmRepo=https://charts.bitnami.com/bitnami&helmChart=postgresql&helmChartVersion=11.9.13"
   "name=mysql&version=8.0&type=mysql&endpoint=tcp:3306"
   "name=activemq-artemis&version=2.26&type=activemq&endpoint=tcp:8161"
   "name=mariadb&version=10.9&type=mariadb&endpoint=tcp:3306"
