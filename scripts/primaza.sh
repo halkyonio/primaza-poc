d10fee1eb3321d64d3488c224b5844642adee84d
Charles Moulliard <cmoulliard@redhat.com>
2023-07-19 17:42:54 +0200

    Add a comment to test the job again. temp_job
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -55,6 +55,7 @@ function primazaUsage() {
 
 function build() {
   #pushd ${PROJECT_DIR}
+  #
   mvn clean install -DskipTests -Dquarkus.container-image.build=true \
      -Dquarkus.container-image.push=true \
      -Dquarkus.container-image.registry=${REGISTRY} \
