b25fc9a3bd8b9b4a94e54e6308a8d1d305defc4f
Charles Moulliard <cmoulliard@redhat.com>
2023-07-24 11:47:49 +0200

    Replace call to p function with note function
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -95,13 +95,13 @@ function deploy() {
       -l app.kubernetes.io/name=primaza-app \
       --timeout=7m"
 
-    p "waiting till Primaza Application is running"
+    note "waiting till Primaza Application is running"
     POD_NAME=$(k get pod -l app.kubernetes.io/name=primaza-app -n ${NAMESPACE} -o name)
     while [[ $(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- bash -c "curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/home") != "200" ]];
       do sleep 1
     done
 
-    p "Get the kubeconf and creating a cluster"
+    note "Get the kubeconf and creating a cluster"
     KIND_URL=https://kubernetes.default.svc
     cmdExec "kind get kubeconfig -n ${CONTEXT_TO_USE} > local-kind-kubeconfig"
     cmdExec "k cp local-kind-kubeconfig ${NAMESPACE}/${POD_NAME:4}:/tmp/local-kind-kubeconfig -c primaza-app"
@@ -109,7 +109,7 @@ function deploy() {
     RESULT=$(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- sh -c "curl -X POST -H 'Content-Type: multipart/form-data' -F name=local-kind -F excludedNamespaces=$NS_TO_BE_EXCLUDED -F environment=DEV -F url=$KIND_URL -F kubeConfig=@/tmp/local-kind-kubeconfig -s -i localhost:8080/clusters")
     if [ "$RESULT" = *"500 Internal Server Error"* ]
     then
-        p "Cluster failed to be saved in Primaza: $RESULT"
+        note "Cluster failed to be saved in Primaza: $RESULT"
         k describe $POD_NAME -n ${NAMESPACE}
         k logs $POD_NAME -n ${NAMESPACE}
         exit 1
@@ -138,13 +138,13 @@ function localDeploy() {
       -l app.kubernetes.io/name=primaza-app \
       --timeout=7m"
 
-    p "waiting till Primaza Application is running"
+    note "waiting till Primaza Application is running"
     POD_NAME=$(k get pod -l app.kubernetes.io/name=primaza-app -n ${NAMESPACE} -o name)
     while [[ $(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- bash -c "curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/home") != "200" ]];
       do sleep 1
     done
 
-    p "Get the kubeconf and creating a cluster"
+    note "Get the kubeconf and creating a cluster"
     KIND_URL=https://kubernetes.default.svc
     cmdExec "kind get kubeconfig -n ${CONTEXT_TO_USE} > local-kind-kubeconfig"
     cmdExec "k cp local-kind-kubeconfig ${NAMESPACE}/${POD_NAME:4}:/tmp/local-kind-kubeconfig -c primaza-app"
@@ -152,7 +152,7 @@ function localDeploy() {
     RESULT=$(k exec -i $POD_NAME -c primaza-app -n ${NAMESPACE} -- sh -c "curl -X POST -H 'Content-Type: multipart/form-data' -F name=local-kind -F excludedNamespaces=$NS_TO_BE_EXCLUDED -F environment=DEV -F url=$KIND_URL -F kubeConfig=@/tmp/local-kind-kubeconfig -s -i localhost:8080/clusters")
     if [ "$RESULT" = *"500 Internal Server Error"* ]
     then
-        p "Cluster failed to be saved in Primaza: $RESULT"
+        note "Cluster failed to be saved in Primaza: $RESULT"
         k describe $POD_NAME -n ${NAMESPACE}
         k logs $POD_NAME -n ${NAMESPACE}
         exit 1
