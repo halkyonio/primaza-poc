253536526de291e7f20897e8ca88204f8ba66d48
Charles Moulliard <cmoulliard@redhat.com>
2023-07-25 12:49:49 +0200

    Bind application
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -149,6 +149,40 @@ function localDeploy() {
 
 }
 
+function bindApplication() {
+  APPLICATION_NAME=$1
+  CLAIM_NAME=$2
+
+  POD_NAME=$(kubectl get pod -l app.kubernetes.io/name=primaza-app -n ${NAMESPACE} -o name)
+
+  APPLICATION=$(kubectl exec -i $POD_NAME --container primaza-app -n ${NAMESPACE} -- sh -c "curl -H 'Accept: application/json' -s localhost:8080/applications/name/$APPLICATION_NAME")
+  APPLICATION_ID=$(echo "$APPLICATION" | jq -r '.id')
+  note "Application ID to be bound $APPLICATION_ID"
+
+  CLAIM=$(kubectl exec -i $POD_NAME --container primaza-app -n ${NAMESPACE} -- sh -c "curl -H 'Accept: application/json' -s localhost:8080/claims/name/$CLAIM_NAME")
+  CLAIM_ID=$(echo "$CLAIM" | jq -r '.id')
+  note "Claim ID to be bound $CLAIM_ID"
+
+  RESULT=$(kubectl exec -i $POD_NAME --container primaza-app -n ${NAMESPACE} -- sh -c "curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d 'claimId=$CLAIM_ID' -s -i localhost:8080/applications/claim/$APPLICATION_ID")
+  if [[ "$RESULT" = *"500 Internal Server Error"* ]]; then
+    error "Application failed to be bound in Primaza: $RESULT"
+    exit 1
+  fi
+  if [[ "$RESULT" = *"404 Not Found"* ]]; then
+    error "Application failed to be bound in Primaza: $RESULT"
+    exit 1
+  fi
+  if [[ "$RESULT" = *"406 Not Acceptable"* ]]; then
+    error "Application failed to be bound in Primaza: $RESULT"
+    exit 1
+  fi
+  if [[ "$RESULT" = *"alert-danger"* ]]; then
+    error "Application failed to be bound in Primaza: $RESULT"
+    exit 1
+  fi
+  note "Application bound in Primaza: $RESULT"
+}
+
 function loaddata() {
    note "Creating the cluster's record"
    RESPONSE=$(${SCRIPTS_DIR}/data/cluster.sh)
@@ -189,6 +223,7 @@ case $1 in
     deploy)       "$@"; exit;;
     localdeploy)  localDeploy; exit;;
     loaddata)     loaddata; exit;;
+    bindApplication) "$@"; exit;;
     remove)       "$@"; exit;;
     *)
       build
