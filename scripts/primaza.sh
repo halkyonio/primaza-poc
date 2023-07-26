2dd6b925051d406b0c7a4144de711e5d8fe4c0a0
Charles Moulliard <cmoulliard@redhat.com>
2023-07-26 10:31:05 +0200

    curl in verbose mode to try to understand why we got HTTP 400 error
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- scripts/primaza.sh
+++ scripts/primaza.sh
@@ -159,7 +159,7 @@ function bindApplication() {
   note "Claim ID to be bound: $CLAIM_ID"
 
   note "curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d 'claimId=$CLAIM_ID' -s -i $PRIMAZA_URL/applications/claim/$APPLICATION_ID"
-  RESULT=$(curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d 'claimId=$CLAIM_ID' -s -i $PRIMAZA_URL/applications/claim/$APPLICATION_ID)
+  RESULT=$(curl -v -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d 'claimId=$CLAIM_ID' -s -i $PRIMAZA_URL/applications/claim/$APPLICATION_ID)
   if [[ "$RESULT" = *"500 Internal Server Error"* ]]; then
     error "Application failed to be bound in Primaza: $RESULT"
     exit 1
